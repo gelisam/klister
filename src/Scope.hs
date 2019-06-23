0da3c2ddd819bb647f98ddab8c6672334a1d55c9
Samuel Gélineau <gelisam+github@gmail.com>
2019-06-23 13:23:01 -0400

    lensify Scope

--- src/Scope.hs
+++ src/Scope.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Scope where
+
+import Control.Lens
 
 
 -- Int should be enough for now - consider bumping to something like int64
 newtype Scope = Scope Int
   deriving (Eq, Ord, Show)
+makePrisms ''Scope
 
 nextScope :: Scope -> Scope
 nextScope (Scope i) = Scope (i + 1)
