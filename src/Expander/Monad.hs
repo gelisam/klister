b0418c56fde472c48109046ec369b61d5db5f0e7
David Christiansen <david@davidchristiansen.dk>
2019-07-16 09:27:31 -0700

    Improve organization of expander

---
+++ src/Expander/Monad.hs
@@ -0,0 +1,191 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Expander.Monad where
+
+import Control.Lens
+import Control.Monad.Except
+import Control.Monad.Reader
+
+import Data.IORef
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Unique
+
+import Numeric.Natural
+
+import Core
+import Env
+import Evaluator
+import Module
+import Signals
+import SplitCore
+import Scope
+import ScopeSet (ScopeSet)
+import qualified ScopeSet
+import Syntax
+import Value
+
+newtype Expand a = Expand
+  { runExpand :: ReaderT ExpanderContext (ExceptT ExpansionErr IO) a
+  }
+  deriving (Functor, Applicative, Monad, MonadError ExpansionErr, MonadIO, MonadReader ExpanderContext)
+
+execExpand :: Expand a -> ExpanderContext -> IO (Either ExpansionErr a)
+execExpand act ctx = runExceptT $ runReaderT (runExpand act) ctx
+
+
+data ExpanderTask
+  = Ready SplitCorePtr Syntax
+  | AwaitingSignal SplitCorePtr Signal Value -- the value is the continuation
+  | AwaitingMacro SplitCorePtr TaskAwaitMacro
+  | ReadyDecl DeclPtr Syntax
+  | MoreDecls ModBodyPtr Syntax DeclPtr -- Depends on the binding of the name(s) from the decl
+
+data TaskAwaitMacro = TaskAwaitMacro
+  { awaitMacroBinding :: Binding
+  , awaitMacroDependsOn :: [SplitCorePtr] -- the [Unique] is the collection of open problems that represent the macro's expansion. When it's empty, the macro is available.
+  , awaitMacroLocation :: SplitCorePtr -- the destination into which the macro will be expanded.
+  , awaitMacroSyntax :: Syntax -- the syntax object to be expanded once the macro is available
+  }
+
+instance Show TaskAwaitMacro where
+  show (TaskAwaitMacro _ _ _ stx) = "TaskAwaitMacro " ++ T.unpack (syntaxText stx)
+
+instance Show ExpanderTask where
+  show (Ready _dest stx) = "Ready " ++ T.unpack (syntaxText stx)
+  show (AwaitingSignal _dest on _k) = "AwaitingSignal (" ++ show on ++ ")"
+  show (AwaitingMacro _dest t) = "AwaitingMacro (" ++ show t ++ ")"
+  show (ReadyDecl _dest stx) = "ReadyDecl " ++ T.unpack (syntaxText stx)
+  show (MoreDecls _dest stx _waiting) = "MoreDecls " ++ T.unpack (syntaxText stx)
+
+newtype TaskID = TaskID Unique
+  deriving (Eq, Ord)
+
+newTaskID :: Expand TaskID
+newTaskID = liftIO $ TaskID <$> newUnique
+
+
+data ExpansionErr
+  = Ambiguous Ident
+  | Unknown (Stx Text)
+  | NoProgress [ExpanderTask]
+  | NotIdentifier Syntax
+  | NotEmpty Syntax
+  | NotCons Syntax
+  | NotRightLength Natural Syntax
+  | NotVec Syntax
+  | UnknownPattern Syntax
+  | MacroRaisedSyntaxError (SyntaxError Syntax)
+  | MacroEvaluationError EvalError
+  | ValueNotMacro Value
+  | ValueNotSyntax Value
+  | InternalError String
+  deriving (Show)
+
+expansionErrText :: ExpansionErr -> Text
+expansionErrText (Ambiguous x) = "Ambiguous identifier " <> T.pack (show x)
+expansionErrText (Unknown x) = "Unknown: " <> T.pack (show x)
+expansionErrText (NoProgress tasks) =
+  "No progress was possible: " <> T.pack (show tasks)
+expansionErrText (NotIdentifier stx) =
+  "Not an identifier: " <> syntaxText stx
+expansionErrText (NotEmpty stx) = "Expected (), but got " <> syntaxText stx
+expansionErrText (NotCons stx) =
+  "Expected non-empty parens, but got " <> syntaxText stx
+expansionErrText (NotRightLength len stx) =
+  "Expected " <> T.pack (show len) <>
+  " entries between square brackets, but got " <> syntaxText stx
+expansionErrText (NotVec stx) =
+  "Expected square-bracketed vec but got " <> syntaxText stx
+expansionErrText (UnknownPattern stx) =
+  "Unknown pattern " <> syntaxText stx
+expansionErrText (MacroRaisedSyntaxError err) =
+  "Syntax error from macro: " <> T.pack (show err)
+expansionErrText (MacroEvaluationError err) =
+  "Error during macro evaluation: \n\t" <> evalErrorText err
+expansionErrText (ValueNotMacro val) =
+  "Not a macro monad value: " <> valueText val
+expansionErrText (ValueNotSyntax val) =
+  "Not a syntax object: " <> valueText val
+expansionErrText (InternalError str) =
+  "Internal error during expansion! This is a bug in the implementation." <> T.pack str
+
+newtype Phase = Phase Natural
+  deriving (Eq, Ord, Show)
+
+newtype Binding = Binding Unique
+  deriving (Eq, Ord)
+
+type BindingTable = Map Text [(ScopeSet, Binding)]
+
+newtype ExpansionEnv = ExpansionEnv (Map.Map Binding EValue)
+  deriving (Semigroup, Monoid)
+
+data EValue
+  = EPrimMacro (SplitCorePtr -> Syntax -> Expand ()) -- ^ For "special forms"
+  | EPrimModuleMacro (Syntax -> Expand ())
+  | EPrimDeclMacro (DeclPtr -> Syntax -> Expand ())
+  | EVarMacro !Var -- ^ For bound variables (the Unique is the binding site of the var)
+  | EUserMacro !SyntacticCategory !Value -- ^ For user-written macros
+
+data SyntacticCategory = ModuleMacro | DeclMacro | ExprMacro
+
+
+data ExpanderContext = ExpanderContext
+  { _expanderState :: IORef ExpanderState
+  , _expanderPhase :: !Phase
+  }
+
+mkInitContext :: IO ExpanderContext
+mkInitContext = do
+  st <- newIORef initExpanderState
+  return $ ExpanderContext { _expanderState = st
+                           , _expanderPhase = Phase 0
+                           }
+
+data ExpanderState = ExpanderState
+  { _expanderReceivedSignals :: !(Set.Set Signal)
+  , _expanderEnvironments :: !(Map.Map Phase (Env Value))
+  , _expanderNextScope :: !Scope
+  , _expanderBindingTable :: !BindingTable
+  , _expanderExpansionEnv :: !ExpansionEnv
+  , _expanderTasks :: [(TaskID, ExpanderTask)]
+  , _expanderCompletedCore :: !(Map.Map SplitCorePtr (CoreF SplitCorePtr))
+  , _expanderCompletedModBody :: !(Map.Map ModBodyPtr (ModuleBodyF DeclPtr ModBodyPtr))
+  , _expanderCompletedDecls :: !(Map.Map DeclPtr (Decl SplitCorePtr))
+  , _expanderModuleName :: Maybe ModuleName
+  , _expanderModuleTop :: Maybe ModBodyPtr
+  , _expanderModuleImports :: [Import]
+  , _expanderModuleExports :: [Export]
+  }
+
+initExpanderState :: ExpanderState
+initExpanderState = ExpanderState
+  { _expanderReceivedSignals = Set.empty
+  , _expanderEnvironments = Map.empty
+  , _expanderNextScope = Scope 0
+  , _expanderBindingTable = Map.empty
+  , _expanderExpansionEnv = mempty
+  , _expanderTasks = []
+  , _expanderCompletedCore = Map.empty
+  , _expanderCompletedModBody = Map.empty
+  , _expanderCompletedDecls = Map.empty
+  , _expanderModuleName = Nothing
+  , _expanderModuleTop = Nothing
+  , _expanderModuleImports = []
+  , _expanderModuleExports = []
+  }
+
+makePrisms ''Binding
+makePrisms ''ExpansionErr
+makePrisms ''Phase
+makeLenses ''ExpanderContext
+makeLenses ''ExpanderState
+makePrisms ''EValue
+makePrisms ''SyntacticCategory
+makePrisms ''ExpansionEnv
+makePrisms ''ExpanderTask
